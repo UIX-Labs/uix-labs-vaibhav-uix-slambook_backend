d04d9466bd34fb62f28b277882a55046f7a1dede
vaibhav uix <vaibhav@uixlabs.in>
2023-12-22 08:49:59 +0530

    task: Task 1: Setting up Server

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 // TASK 1: Setting up server
 
 
-// const http = require('http');
+const http = require('http');
 
-// const server = http.createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/plain');
-//   res.end('Hello World\n');
-// });
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
 
-// const PORT = 80;
-// const HOSTNAME = '0.0.0.0';
+const PORT = 80;
+const HOSTNAME = '0.0.0.0';
 
-// server.listen(PORT, HOSTNAME, () => {
-//   console.log(`Server running at http://${HOSTNAME}:${PORT}/`);
-// });
+server.listen(PORT, HOSTNAME, () => {
+  console.log(`Server running at http://${HOSTNAME}:${PORT}/`);
+});
 
 
 
@@ -184,116 +184,116 @@ server.listen(PORT, HOSTNAME, () => {
 
 
 //TASK 06-09 CRUD For Slambook: 
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
 
-const PORT = 80;
-const HOSTNAME = '0.0.0.0';
+// const PORT = 80;
+// const HOSTNAME = '0.0.0.0';
 
-const app = express();
+// const app = express();
 
-// Middleware
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(cors());
+// // Middleware
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.use(cors());
 
-// MongoDB connection
-const uri = "mongodb+srv://suraj_admin:suraj_admin@cluster0.dme40pl.mongodb.net/?retryWrites=true&w=majority";
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((err) => {
-    console.error('Could not connect to MongoDB:', err);
-    process.exit(1);
-  });
+// // MongoDB connection
+// const uri = "mongodb+srv://suraj_admin:suraj_admin@cluster0.dme40pl.mongodb.net/?retryWrites=true&w=majority";
+// mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => {
+//     console.log('Connected to MongoDB');
+//   })
+//   .catch((err) => {
+//     console.error('Could not connect to MongoDB:', err);
+//     process.exit(1);
+//   });
 
-// SlamBook schema and model
-const slambookSchema = new mongoose.Schema({
-  nameInYourContact: String,
-  relationship: String,
-  somethingYouLikeInMe: String,
-  somethingYouHateInMe: String,
-  ifIDieYourReaction: String,
-  whatDidYouFeelWhenYouFirstSawMe: String,
-  beutifulMessageForMe: String,
-  nickNameForMe: String,
-  songDedicatedToMe: String,
-  canIShare: String,
-  yourName: String
-});
+// // SlamBook schema and model
+// const slambookSchema = new mongoose.Schema({
+//   nameInYourContact: String,
+//   relationship: String,
+//   somethingYouLikeInMe: String,
+//   somethingYouHateInMe: String,
+//   ifIDieYourReaction: String,
+//   whatDidYouFeelWhenYouFirstSawMe: String,
+//   beutifulMessageForMe: String,
+//   nickNameForMe: String,
+//   songDedicatedToMe: String,
+//   canIShare: String,
+//   yourName: String
+// });
 
-const SlamBook = mongoose.model('SlamBook', slambookSchema);
-// Routes
-app.get('/', (req, res) => {
-  res.json({ message: `Yay, app is running hvdhbvhjdbf on port ${PORT}` });
-});
+// const SlamBook = mongoose.model('SlamBook', slambookSchema);
+// // Routes
+// app.get('/', (req, res) => {
+//   res.json({ message: `Yay, app is running hvdhbvhjdbf on port ${PORT}` });
+// });
 
-app.get('/slambook', async (req, res) => {
-  try {
-    const entries = await SlamBook.find(req.query);
-    res.json(entries);
-  } catch (error) {
-    console.error('Error fetching slambook entries:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
+// app.get('/slambook', async (req, res) => {
+//   try {
+//     const entries = await SlamBook.find(req.query);
+//     res.json(entries);
+//   } catch (error) {
+//     console.error('Error fetching slambook entries:', error);
+//     res.status(500).json({ error: 'Internal Server Error' });
+//   }
+// });
 
-app.post('/slambook', async (req, res) => {
-  try {
-    const newEntry = new SlamBook(req.body);
-    await newEntry.save();
-    res.status(201).json({ message: 'Entry Created' });
-  } catch (error) {
-    console.error('Error creating slambook entry:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
+// app.post('/slambook', async (req, res) => {
+//   try {
+//     const newEntry = new SlamBook(req.body);
+//     await newEntry.save();
+//     res.status(201).json({ message: 'Entry Created' });
+//   } catch (error) {
+//     console.error('Error creating slambook entry:', error);
+//     res.status(500).json({ error: 'Internal Server Error' });
+//   }
+// });
 
-app.get('/slambook/:id', async (req, res) => {
-  try {
-    const entry = await SlamBook.findById(req.params.id);
-    if (entry) {
-      res.json(entry);
-    } else {
-      res.status(404).json({ message: 'Entry not found' });
-    }
-  } catch (error) {
-    console.error('Error fetching slambook entry:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
+// app.get('/slambook/:id', async (req, res) => {
+//   try {
+//     const entry = await SlamBook.findById(req.params.id);
+//     if (entry) {
+//       res.json(entry);
+//     } else {
+//       res.status(404).json({ message: 'Entry not found' });
+//     }
+//   } catch (error) {
+//     console.error('Error fetching slambook entry:', error);
+//     res.status(500).json({ error: 'Internal Server Error' });
+//   }
+// });
 
-app.put('/slambook/:id', async (req, res) => {
-  try {
-    const updatedEntry = await SlamBook.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    if (updatedEntry) {
-      res.json({ message: 'Entry Updated' });
-    } else {
-      res.status(404).json({ message: 'Entry not found' });
-    }
-  } catch (error) {
-    console.error('Error updating slambook entry:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
+// app.put('/slambook/:id', async (req, res) => {
+//   try {
+//     const updatedEntry = await SlamBook.findByIdAndUpdate(req.params.id, req.body, { new: true });
+//     if (updatedEntry) {
+//       res.json({ message: 'Entry Updated' });
+//     } else {
+//       res.status(404).json({ message: 'Entry not found' });
+//     }
+//   } catch (error) {
+//     console.error('Error updating slambook entry:', error);
+//     res.status(500).json({ error: 'Internal Server Error' });
+//   }
+// });
 
-app.delete('/slambook/:id', async (req, res) => {
-  try {
-    const deletedEntry = await SlamBook.findByIdAndDelete(req.params.id);
-    if (deletedEntry) {
-      res.json({ message: 'Entry Deleted' });
-    } else {
-      res.status(404).json({ message: 'Entry not found' });
-    }
-  } catch (error) {
-    console.error('Error deleting slambook entry:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
-// Start the server
-app.listen(PORT, HOSTNAME, () => {
-  console.log(`Server running at http://${HOSTNAME}:${PORT}/`);
-});
+// app.delete('/slambook/:id', async (req, res) => {
+//   try {
+//     const deletedEntry = await SlamBook.findByIdAndDelete(req.params.id);
+//     if (deletedEntry) {
+//       res.json({ message: 'Entry Deleted' });
+//     } else {
+//       res.status(404).json({ message: 'Entry not found' });
+//     }
+//   } catch (error) {
+//     console.error('Error deleting slambook entry:', error);
+//     res.status(500).json({ error: 'Internal Server Error' });
+//   }
+// });
+// // Start the server
+// app.listen(PORT, HOSTNAME, () => {
+//   console.log(`Server running at http://${HOSTNAME}:${PORT}/`);
+// });
